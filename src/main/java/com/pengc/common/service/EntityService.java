2bcc65e0906f5140bd94311f5b81a98d1d23e546
pengcheng0905 <pengchenggo@gmail.com>
2016-12-06 19:02:30 +0900

    code improve

--- src/main/java/com/pengc/common/service/EntityService.java
+++ src/main/java/com/pengc/common/service/EntityService.java
@@ -1,10 +1,11 @@
 package com.pengc.common.service;
 
+import javax.persistence.Embeddable;
+
 import com.pengc.common.dao.EntityDao;
 import com.pengc.common.model.EntityModel;
 
-//@Service
-//@Transactional(readOnly = false)
+@Embeddable
 public abstract class EntityService<T extends EntityModel> {
 
 	public abstract EntityDao<T> getEntityDao();
