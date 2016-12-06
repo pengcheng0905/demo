1637c03d606d6c60b5238098e4c509ac04e6b761
pengcheng0905 <pengchenggo@gmail.com>
2016-12-06 18:51:14 +0900

    remove unused file

--- src/main/java/com/pengc/patlite/utils/Alarm.java
+++ src/main/java/com/pengc/patlite/utils/Alarm.java
@@ -3,33 +3,33 @@ package com.pengc.patlite.utils;
 public interface Alarm {
 
 	// buzzer on
-	public abstract boolean buzzerOn(PatliteFlashType type);
+	public boolean buzzerOn(PatliteFlashType type);
 
 	// buzzer off
-	public abstract boolean buzzerOff();
+	public boolean buzzerOff();
 
 	// red LED on
-	public abstract boolean redOn(PatliteFlashType type);
+	public boolean redOn(PatliteFlashType type);
 
 	// red LED off
-	public abstract boolean redOff();
+	public boolean redOff();
 
 	// yellow LED on
-	public abstract boolean yellowOn(PatliteFlashType type);
+	public boolean yellowOn(PatliteFlashType type);
 
 	// yellow LED off
-	public abstract boolean yellowOff();
+	public boolean yellowOff();
 
 	// green LED on
-	public abstract boolean greenOn(PatliteFlashType type);
+	public boolean greenOn(PatliteFlashType type);
 
 	// green LED off
-	public abstract boolean greenOff();
+	public boolean greenOff();
 
 	// turn off everything.
-	public abstract boolean setOffAll();
+	public boolean setOffAll();
 
 	// set alarm at once
-	public abstract boolean setAlarm(boolean red, boolean yellow, boolean green, boolean buzzer);
+	public boolean setAlarm(boolean red, boolean yellow, boolean green, boolean buzzer);
 
 }
